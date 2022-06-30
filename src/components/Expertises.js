23765034bb7e6517d283d0b6e4ed0d647bb94f67
Rodolphe Breton <rodolphe.breton@netgem.com>
2022-06-30 10:25:19 +0200

    Added new pages + worked on UI + updated packages

---
+++ src/components/Expertises.js
@@ -0,0 +1,12 @@
+/* @flow */
+
+import './Expertise.scss';
+import * as React from 'react';
+
+const Expertise = (): React.Node => (
+  <div className='expertise'>
+    <div>Expertise</div>
+  </div>
+);
+
+export default Expertise;
