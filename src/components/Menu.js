c926bb1b19de10c1a6d61661c0fc7e0b285c5b2b
Rodolphe Breton <rodolphe.breton@netgem.com>
2023-05-16 07:32:07 +0200

    Expand menu items on keyboard navigation (close #45)

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,9 +2,11 @@
 
 import './Menu.scss';
 import * as React from 'react';
-import {useCallback, useEffect, useState} from 'react';
+import {useCallback, useState} from 'react';
+import FocusLock from 'react-focus-lock';
 import {HashLink} from 'react-router-hash-link';
 import {Localizer} from '../helpers/localizer';
+import {RemoveScroll} from 'react-remove-scroll';
 import Social from './Social';
 import {scrollTop} from '../helpers/scroll';
 import {useSelector} from 'react-redux';
@@ -14,15 +16,22 @@ const TWO = 2;
 
 const renderHeader = (handleCloseOnClick: (event: SyntheticMouseEvent<HTMLElement> | SyntheticTouchEvent<HTMLElement>) => void) => (
   <div className='menu__header'>
-    <img
-      alt='Close button'
-      className='close'
-      draggable={false}
-      onClick={handleCloseOnClick}
-      src={x} />
-    <div
-      className='menu menu__closed menu__label'
-      onClick={handleCloseOnClick}>{Localizer.localize('menu.label')}</div>
+    <button
+      className='no-style-button close'
+      type='button'>
+      <img
+        alt='Close button'
+        draggable={false}
+        onClick={handleCloseOnClick}
+        src={x} />
+    </button>
+    <button
+      className=' no-style-button '
+      type='button'>
+      <div
+        className='menu menu__closed menu__label'
+        onClick={handleCloseOnClick}>{Localizer.localize('menu.label')}</div>
+    </button>
   </div>
 );
 
@@ -30,7 +39,9 @@ const renderLinks = (
   language: string,
   expandedStates: Array<boolean>,
   toggleSection: (event: SyntheticMouseEvent<HTMLElement> | SyntheticTouchEvent<HTMLElement>) => void,
-  handleCloseOnClick: () => void
+  handleCloseOnClick: () => void,
+  handleOnFocus: (event: SyntheticKeyboardEvent<HTMLElement>) => void,
+  handleCloseAll: () => void
 ) => (
   <nav className='menu__links'>
     <div
@@ -40,10 +51,12 @@ const renderLinks = (
     <div className={`menu__item_accordion ${expandedStates[0] ? 'expanded' : ''}`}>
       <HashLink
         onClick={handleCloseOnClick}
+        onFocus={handleOnFocus}
         scroll={scrollTop}
         to={`/${language}/home#first-name`}>{Localizer.localize('menu.agency.first_name')}</HashLink>
       <HashLink
         onClick={handleCloseOnClick}
+        onFocus={handleOnFocus}
         scroll={scrollTop}
         to={`/${language}/home#philosophy`}>{Localizer.localize('menu.agency.philosophy')}</HashLink>
       {/* <HashLink */}
@@ -52,10 +65,12 @@ const renderLinks = (
       {/* DEBUG  to={`/${language}/home#manifest`}>{Localizer.localize('menu.agency.manifest')}</HashLink> */}
       <HashLink
         onClick={handleCloseOnClick}
+        onFocus={handleOnFocus}
         scroll={scrollTop}
         to={`/${language}/home#founder`}>{Localizer.localize('menu.agency.founder')}</HashLink>
       <HashLink
         onClick={handleCloseOnClick}
+        onFocus={handleOnFocus}
         scroll={scrollTop}
         to={`/${language}/home#creative-director`}>{Localizer.localize('menu.agency.creative_director')}</HashLink>
     </div>
@@ -66,22 +81,27 @@ const renderLinks = (
     <div className={`menu__item_accordion ${expandedStates[1] ? 'expanded' : ''}`}>
       <HashLink
         onClick={handleCloseOnClick}
+        onFocus={handleOnFocus}
         scroll={scrollTop}
         to={`/${language}/expertises#strategy`}>{Localizer.localize('menu.expertises.strategy')}</HashLink>
       <HashLink
         onClick={handleCloseOnClick}
+        onFocus={handleOnFocus}
         scroll={scrollTop}
         to={`/${language}/expertises#creation`}>{Localizer.localize('menu.expertises.creation')}</HashLink>
       <HashLink
         onClick={handleCloseOnClick}
+        onFocus={handleOnFocus}
         scroll={scrollTop}
         to={`/${language}/expertises#digital`}>{Localizer.localize('menu.expertises.digital')}</HashLink>
       <HashLink
         onClick={handleCloseOnClick}
+        onFocus={handleOnFocus}
         scroll={scrollTop}
         to={`/${language}/expertises#edition`}>{Localizer.localize('menu.expertises.edition')}</HashLink>
       <HashLink
         onClick={handleCloseOnClick}
+        onFocus={handleOnFocus}
         scroll={scrollTop}
         to={`/${language}/expertises#production`}>{Localizer.localize('menu.expertises.production')}</HashLink>
     </div>
@@ -92,16 +112,19 @@ const renderLinks = (
     <div className={`menu__item_accordion ${expandedStates[TWO] ? 'expanded' : ''}`}>
       <HashLink
         onClick={handleCloseOnClick}
+        onFocus={handleOnFocus}
         scroll={scrollTop}
         to={`/${language}/creation#projects`}>{Localizer.localize('menu.creation.projects')}</HashLink>
       <HashLink
         onClick={handleCloseOnClick}
+        onFocus={handleOnFocus}
         scroll={scrollTop}
         to={`/${language}/creation#customers`}>{Localizer.localize('menu.creation.customers')}</HashLink>
     </div>
     <HashLink
       className='menu__item'
       onClick={handleCloseOnClick}
+      onFocus={handleCloseAll}
       to={`/${language}/contact#top`}>{Localizer.localize('menu.contact.label')}</HashLink>
   </nav>
 );
@@ -143,28 +166,29 @@ const Menu = (): React.Node => {
     const newStates = [false, false, false];
     newStates[index] = !expandedStates[index];
     setExpandedStates(newStates);
-  }, [expandedStates, setExpandedStates]);
+  }, [expandedStates]);
 
   const handleCloseOnClick = useCallback(() => {
     setIsOpen(false);
   }, [setIsOpen]);
 
-  // Initialization
-  useEffect(() => {
-    // Close menu when user clicks outside it
-    const onMouseUp = (event: SyntheticMouseEvent<HTMLElement> | SyntheticTouchEvent<HTMLElement>) => {
-      const {target} = event;
+  const handleOnFocus = useCallback((event: SyntheticKeyboardEvent<HTMLElement>) => {
+    const toggleElt = event.currentTarget.parentElement?.previousElementSibling;
 
-      if (target instanceof HTMLElement && !target.closest('.menu')) {
-        setIsOpen(false);
-      }
-    };
+    if (!toggleElt) {
+      return;
+    }
 
-    window.addEventListener('mouseup', onMouseUp);
+    const index = Number(toggleElt.getAttribute('data-index'));
+    if (!expandedStates[index]) {
+      const newStates = [false, false, false];
+      newStates[index] = true;
+      setExpandedStates(newStates);
+    }
+  }, [expandedStates]);
 
-    return () => {
-      window.removeEventListener('mouseup', onMouseUp);
-    };
+  const handleCloseAll = useCallback(() => {
+    setExpandedStates([false, false, false]);
   }, []);
 
   const languageOnChange = useCallback((event: SyntheticInputEvent<HTMLElement>) => {
@@ -185,11 +209,24 @@ const Menu = (): React.Node => {
   }
 
   return (
-    <div className='menu menu__opened'>
-      {renderHeader(handleCloseOnClick)}
-      {renderLinks(language, expandedStates, toggleSection, handleCloseOnClick)}
-      {renderFooter(language, languageOnChange)}
-    </div>
+    <FocusLock returnFocus>
+      <RemoveScroll>
+        <div className='menu__wrapper'>
+          <div
+            className='menu__overlay'
+            onClick={handleCloseOnClick} />
+          <div
+            aria-label='menu'
+            aria-modal='true'
+            className='menu menu__opened'
+            role='dialog'>
+            {renderHeader(handleCloseOnClick)}
+            {renderLinks(language, expandedStates, toggleSection, handleCloseOnClick, handleOnFocus, handleCloseAll)}
+            {renderFooter(language, languageOnChange)}
+          </div>
+        </div>
+      </RemoveScroll>
+    </FocusLock>
   );
 };
 
