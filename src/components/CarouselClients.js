51ccc6dc795918952c99c51d42ed55bfe027eedf
Rodolphe Breton <rodolphe.breton@netgem.com>
2023-05-24 14:34:06 +0200

    Removed last slide of clients carousel

--- src/components/CarouselClients.js
+++ src/components/CarouselClients.js
@@ -3,19 +3,8 @@
 import './CarouselClients.scss';
 import * as React from 'react';
 import Carousel from './Carousel';
-import {Localizer} from '../helpers/localizer';
 
-export const PAGE_COUNT = 6;
-
-const renderSlideContent = (index: number): React.Element<any> | null => {
-  if (index < PAGE_COUNT - 1) {
-    return null;
-  }
-
-  return (
-    <div className='slide__text-content'>{Localizer.localize(`clients.page${index + 1}.text`)}</div>
-  );
-};
+export const PAGE_COUNT = 5;
 
 const CarouselClients = (): React.Node => (
   <Carousel
@@ -23,8 +12,7 @@ const CarouselClients = (): React.Node => (
     id='carousel-clients'
     logo
     pageCount={PAGE_COUNT}
-    scrollLink='carousel-manifest'
-    slideContentRenderer={renderSlideContent} />
+    scrollLink='carousel-manifest' />
 );
 
 export default CarouselClients;
